1f1aaa8bc8fe4e4a429d5ff2f64264eb1f29df80
Chaos <ChaosCodePractice@gmail.com>
2021-10-13 19:23:29 +0100

    Utility functions

--- Auto.py
+++ Auto.py
@@ -5,3 +5,20 @@ import time
 
 s = Stockfish("/Users/44751/Desktop/stockfish.exe")
 s.set_skill_level(20)
+
+#Utility Functions
+def colour(FEN):
+    """
+    Gets colour from FEN 
+    """
+    fen = FEN.split(" ")
+    if fen[1] == "w":
+        return "White"
+    return "Black"
+
+def createTable(c):
+    c.execute("""CREATE TABLE puzzles(
+                FEN text,
+                Move text,
+                Counter integer
+                )""")
